a612bf4b844a68c710796c238db98b1e180f0b45
luzolmedo-cmd <luz.olmedo@escola.pr.gov.br>
2026-06-24 01:24:56 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Mensagem de boas-vindas ao carregar o site
+window.onload = function() {
+    alert("Bem-vindo ao trabalho da Luz Milagros sobre Agro Sustentável!");
+    console.log("Site carregado com sucesso para o Professor Victor.");
+};
+
